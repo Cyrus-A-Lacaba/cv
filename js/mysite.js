c7535ab3baa4a40848288ef467da6781487332d5
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-03-29 13:09:21 +0800

    Add files via upload

--- js/mysite.js
+++ js/mysite.js
@@ -1,15 +1,14 @@
 
 window.onscroll = function() {myFunction()};
+
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    $(".fixed-top").css('opacity', '80%');
+    $(".fixed-top").css('opacity', '90%');
     $(".fixed-top").css('background-color', '#cdd0cb');
-    $(".nav-item").css('visibility', 'hidden');
     $("#Home-btn").css('visibility', 'visible');
   } else {
     $(".fixed-top").css('opacity', '100%');
     $(".fixed-top").css('background-color', '');
-    $(".nav-item").css('visibility', 'visible');
     $("#Home-btn").css('visibility', 'hidden');
   }
 }
