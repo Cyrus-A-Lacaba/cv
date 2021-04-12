e9f1d62081a0118d6203e0210517561978203eab
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-12 11:05:20 +0800

    Add files via upload

--- js/mysite.js
+++ js/mysite.js
@@ -1,14 +1,21 @@
-
+window.onload = function() {$("a.float").addClass("home-visibility")};
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    $(".fixed-top").css('opacity', '90%');
-      $(".fixed-top").css('background-color', '#a1cae2');
-    $("#Home-btn").css('visibility', 'visible');
+    $(".navbar").addClass("page-scrolling");
+    $(".navbar").removeClass("bg-light");
+    $("a.float").removeClass("home-visibility");
   } else {
-    $(".fixed-top").css('opacity', '100%');
-      $(".fixed-top").css('background-color', '');
-    $("#Home-btn").css('visibility', 'hidden');
+    $(".navbar").removeClass("page-scrolling");
+    $("a.float").addClass("home-visibility");
   }
 }
+
+$(".skills-bar").mousedown(function(){
+  $(".skills-bar").addClass("animate-skills");
+});
+
+$(".skills-bar").mouseup(function(){
+  $(".skills-bar").removeClass("animate-skills");
+});
