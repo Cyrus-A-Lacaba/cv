7a0c63f73c47d5a13e24682726a4cc522f2ddcb6
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-12 18:14:02 +0800

    Add files via upload

--- js/mysite.js
+++ js/mysite.js
@@ -1,14 +1,18 @@
-window.onload = function() {$("a.float").addClass("home-visibility")};
+var i;
+window.onload = function() {
+  $("a.float").addClass("home-visibility")
+  $(".to-collapse").slideUp();
+};
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     $(".navbar").addClass("page-scrolling");
     $(".navbar").removeClass("bg-light");
-    $("a.float").removeClass("home-visibility");
+    $("a.float").fadeIn();
   } else {
     $(".navbar").removeClass("page-scrolling");
-    $("a.float").addClass("home-visibility");
+    $("a.float").fadeOut();
   }
 }
 
@@ -19,3 +23,23 @@ $(".skills-bar").mousedown(function(){
 $(".skills-bar").mouseup(function(){
   $(".skills-bar").removeClass("animate-skills");
 });
+
+for (let i=1;i < 7;i++) {
+  $(".co0" + i).on("click",function(){
+    $("tr.co0" + i).slideToggle();
+  });
+
+  $(".co0" + i).on("tap",function(){
+    $("tr.co0" + i).slideToggle();
+  });
+}
+
+for (let i=1;i < 5;i++) {
+  $(".ed0" + i).on("click",function(){
+    $("tr.ed0" + i).slideToggle();
+  });
+
+  $(".ed0" + i).on("tap",function(){
+    $("tr.ed0" + i).slideToggle();
+  });
+}
