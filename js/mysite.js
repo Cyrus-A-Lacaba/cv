ab1104ec2b52db4a7c8038f860e6fd60ad12ad24
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-13 14:04:50 +0800

    Add files via upload

--- js/mysite.js
+++ js/mysite.js
@@ -7,11 +7,8 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    $(".navbar").addClass("page-scrolling");
-    $(".navbar").removeClass("bg-light");
     $("a.float").fadeIn();
-  } else {
-    $(".navbar").removeClass("page-scrolling");
+  } else {    
     $("a.float").fadeOut();
   }
 }
