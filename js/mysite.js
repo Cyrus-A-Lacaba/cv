b371bf9cf4071842a3cf41fa88dc59a532a9569e
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-15 12:58:17 +0800

    Add files via upload

--- js/mysite.js
+++ js/mysite.js
@@ -8,7 +8,7 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     $("a.float").fadeIn();
-  } else {    
+  } else {
     $("a.float").fadeOut();
   }
 }
@@ -24,19 +24,37 @@ $(".skills-bar").mouseup(function(){
 for (let i=1;i < 7;i++) {
   $(".co0" + i).on("click",function(){
     $("tr.co0" + i).slideToggle();
+    toggleArrow("td.company.co0" + i + " i");
   });
 
   $(".co0" + i).on("tap",function(){
     $("tr.co0" + i).slideToggle();
+    toggleArrow("td.company.co0" + i + " i");
   });
 }
 
 for (let i=1;i < 5;i++) {
   $(".ed0" + i).on("click",function(){
     $("tr.ed0" + i).slideToggle();
+    toggleArrow("td.company.ed0" + i + " i");
   });
 
   $(".ed0" + i).on("tap",function(){
     $("tr.ed0" + i).slideToggle();
+    toggleArrow("td.company.ed0" + i + " i");
   });
+}
+
+function toggleArrow(classVal) {
+  if ($(classVal).hasClass("fa-angle-down")) {
+    $(classVal).removeClass("fa-angle-down");
+    $(classVal).addClass("fa-angle-up");
+    return;
+  }
+
+  if ($(classVal).hasClass("fa-angle-up")) {
+    $(classVal).removeClass("fa-angle-up");
+    $(classVal).addClass("fa-angle-down");
+    return;
+  }
 }
